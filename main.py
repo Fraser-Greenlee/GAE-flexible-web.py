73cb3284701a9ff60a3626c801714b2ec862cc0d
Fraser-Greenlee <fraser.greenlee@icloud.com>
2016-04-19 20:20:34 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import web
+
+urls = (
+    '/(.*)', 'hello'
+)
+
+class hello:        
+    def GET(self, name):
+        if not name: 
+            name = 'World'
+        return 'Hello, ' + name + '!'
+
+class MyApplication(web.application):
+    def run(self, port=8080, *middleware):
+        func = self.wsgifunc(*middleware)
+        return web.httpserver.runsimple(func, ('0.0.0.0', port))
+
+app = MyApplication(urls, globals())
+app.run(port=8080)
