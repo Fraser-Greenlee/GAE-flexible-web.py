d4068d55a4611a07f58864c9fdbc53d80ebdb5f0
Fraser-Greenlee <fraser.greenlee@icloud.com>
2016-04-19 21:28:09 +0100

    whoops

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ class hello:
             name = 'World'
         return 'Hello, ' + name + '!'
 
-class MyApplication(web.application):
-    def run(self, port=8080, *middleware):
-        func = self.wsgifunc(*middleware)
-        return web.httpserver.runsimple(func, ('0.0.0.0', port))
+if __name__ == "__main__":
+	class MyApplication(web.application):
+		def run(self, port=8080, *middleware):
+			func = self.wsgifunc(*middleware)
+			return web.httpserver.runsimple(func, ('0.0.0.0', port))
 
-app = MyApplication(urls, globals())
-app.run(port=8080)
+	app = MyApplication(urls, globals())
+	app.run(port=8080)
